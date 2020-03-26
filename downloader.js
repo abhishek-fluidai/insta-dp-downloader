47146f18345c1771958364e0dd55b76f4ef465b0
mabhishek7081 <mabhishek7081@gmail.com>
2020-03-26 19:24:09 +0530

    first commit

---
+++ downloader.js
@@ -0,0 +1,29 @@
+var nightmare = require("nightmare")();
+const download = require("image-downloader");
+
+checkPrice();
+
+async function checkPrice() {
+  const dp = await nightmare
+    .goto("https://www.instagram.com/mauryavansi_saurabh")
+    .wait(".be6sR")
+    .evaluate(() => document.querySelector(".be6sR").src)
+    .end();
+  //   console.log(dp);
+
+  const options = {
+    url: dp,
+    dest: "/home/abhishek/Desktop/"
+  };
+
+  async function downloadIMG() {
+    try {
+      const { filename, image } = await download.image(options);
+      console.log(filename); // => /path/to/dest/image.jpg
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  downloadIMG();
+}
